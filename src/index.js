3150c6f9defa5bfe68d4bda4390b5b4b84924c41
VasylBarna <vasylbarna@gmail.com>
2021-04-22 22:33:37 +0300

    npm instal

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import validatePasword from "./js/validate-password";
-import { addUser } from "./js/api-service";
+// import validatePasword from "./js/validate-password";
+// import { addUser } from "./js/api-service";
 
-import onOpenModal from "./js/on-open-modal";
+// import onOpenModal from "./js/on-open-modal";
 
-onOpenModal.addEventListener("click", onOpenModal);
+// onOpenModal.addEventListener("click", onOpenModal);
 
-addUser("mango");
+// addUser("mango");
 
 // console.log(validatePasword('qweqweqeqweqweqweqw'));
 // import {
@@ -27,3 +27,22 @@ addUser("mango");
 // console.log(name);
 
 // console.log(apiService);
+
+// const Joi = require("joi");
+
+// const shortid = require("shortid");
+
+// const passwordShema = Joi.string().min(3).max(10).alphanum();
+// console.log(passwordShema.validate('barel'));
+
+// console.log(shortid.generate());
+
+// const express = require('express');
+// const app = express();
+// app.use('*', (req, res) => {
+//     console.log('Бил запрос от браузера');
+//     res.send('<h1>Привет Медвед</h1>');
+// });
+// const listener = app.listen(1234, () => {
+//     console.log(`Вебсервер запущен на порте ${listener.address().port}`)
+// });

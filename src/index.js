ecb45f0048313a00a02e80b2cf15c0e50cce63ee
VasylBarna <vasylbarna@gmail.com>
2021-04-23 12:09:40 +0300

    modul9repeta1

--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-// import validatePasword from "./js/validate-password";
-// import { addUser } from "./js/api-service";
+import validatePasword from "./js/validate-password";
+import { addUser } from "./js/api-service";
 
-// import onOpenModal from "./js/on-open-modal";
+import onOpenModal from "./js/on-open-modal";
 
-// onOpenModal.addEventListener("click", onOpenModal);
+onOpenModal.addEventListener("click", onOpenModal);
 
-// addUser("mango");
+addUser("mango");
 
-// console.log(validatePasword('qweqweqeqweqweqweqw'));
-// import {
-//   fetchAllUsers,
-//   fetchUserById,
-//   updateUserById,
-//   x as value,
-//   y as name,
-// } from './js/api-service';
-// import * as apiService from './js/api-service';
+console.log(validatePasword('qweqweqeqweqweqweqw'));
+import {
+  fetchAllUsers,
+  fetchUserById,
+  updateUserById,
+  x as value,
+  y as name,
+} from './js/api-service';
+import * as apiService from './js/api-service';
 
-// console.log(validatePasword);
-// console.log(validatePasword('qweqweqwe'));
+console.log(validatePasword);
+console.log(validatePasword('qweqweqwe'));
 
-// console.log(fetchAllUsers);
-// console.log(fetchUserById);
-// console.log(updateUserById);
-// console.log(value);
-// console.log(name);
+console.log(fetchAllUsers);
+console.log(fetchUserById);
+console.log(updateUserById);
+console.log(value);
+console.log(name);
 
-// console.log(apiService);
+console.log(apiService);
 
-// const Joi = require("joi");
+const Joi = require("joi");
 
-// const shortid = require("shortid");
+const shortid = require("shortid");
 
-// const passwordShema = Joi.string().min(3).max(10).alphanum();
-// console.log(passwordShema.validate('barel'));
+const passwordShema = Joi.string().min(3).max(10).alphanum();
+console.log(passwordShema.validate('barel'));
 
-// console.log(shortid.generate());
+console.log(shortid.generate());
 
-// const express = require('express');
-// const app = express();
-// app.use('*', (req, res) => {
-//     console.log('Бил запрос от браузера');
-//     res.send('<h1>Привет Медвед</h1>');
-// });
-// const listener = app.listen(1234, () => {
-//     console.log(`Вебсервер запущен на порте ${listener.address().port}`)
-// });
+const express = require('express');
+const app = express();
+app.use('*', (req, res) => {
+    console.log('Бил запрос от браузера');
+    res.send('<h1>Привет Медвед</h1>');
+});
+const listener = app.listen(1234, () => {
+    console.log(`Вебсервер запущен на порте ${listener.address().port}`)
+});
